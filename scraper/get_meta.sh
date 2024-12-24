76a14093a4f10331ccf15baf7b9062d212016c0e
kk-macbook <deceiver0123@yahoo.co.jp>
2024-12-25 01:14:44 +0900

    add uncompleted scraper

---
+++ scraper/get_meta.sh
@@ -0,0 +1,2 @@
+kaggle datasets download -d kaggle/meta-kaggle -f Episodes.csv --unzip -p ../data/meta/raw
+kaggle datasets download -d kaggle/meta-kaggle -f EpisodeAgents.csv --unzip -p ../data/meta/raw
